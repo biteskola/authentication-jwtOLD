74b17c7fcac51c5b9b6a8f17d94dcd9d823c35ee
aitorjs <aitiba@gmail.com>
2022-02-25 00:32:54 +0100

    dist kept out

--- dist/__tests__/fixtures/datasources/db.datasource.d.ts
+++
@@ -1,9 +0,0 @@
-import { juggler } from '@loopback/repository';
-export declare class DbDataSource extends juggler.DataSource {
-    static dataSourceName: string;
-    static readonly defaultConfig: {
-        name: string;
-        connector: string;
-    };
-    constructor(dsConfig?: object);
-}
